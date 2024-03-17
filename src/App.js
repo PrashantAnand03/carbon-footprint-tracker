5a3b18a9269f936b488ba225abece8a7942ecb5e
Prashant Anand <prashantanandcse2025@gmail.com>
2024-03-17 20:44:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/NavBar/navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Workflow from "./pages/Workflow/workflow";
+import Tracker from "./pages/Tracker/tracker";
+import Login from "./pages/Login/login";
+import Register from "./pages/Register/register";
+
 
 function App() {
+  const user = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}>Home</Route>
+          <Route exact path="/workflow" element={<Workflow />}>Workflow</Route>
+          <Route exact path="/tracker" element={<Tracker />}>Tracker</Route>
+          <Route path="/login" element={user ? <Home /> : <Login />}>Login</Route>
+          <Route path="/register" element={user ? <Home /> : <Register />}>Register</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
